6381b432c498d62f40f54e0702eb16ce58fc3abe
mMiguelAngelsp <miguel.correopers@gmail.com>
2023-03-16 14:27:33 +0100

    another commit

--- build/install.sh
+++ build/install.sh
@@ -1,3 +1,3 @@
-docker compose -f /var/lib/jenkins/workspace/Job-pCine/docker-compose.yml build 
+docker compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/pCine/api_nest/docker-compose.yml build 
 # docker compose -f /var/lib/jenkins/workspace/Job-EntradasATuAlcance-Backend/docker-compose.yml down 
-docker compose -f /var/lib/jenkins/workspace/Job-EntradasATuAlcance-Backend/docker-compose.yml up -d
+docker compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/pCine/api_nest/docker-compose.yml up -d

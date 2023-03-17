68f7d8543d8b157dd57c86b54987936b312b781e
mMiguelAngelsp <miguel.correopers@gmail.com>
2023-03-17 14:12:02 +0100

    another commit

--- build/install.sh
+++ build/install.sh
@@ -1,3 +1,7 @@
+cd var/lib/jenkins/workspace/Proyectos/Job-pCine/api/
+
+export $(cat .env | xargs)
+
 docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api/docker-compose.yml build --no-cache
 # docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api/docker-compose.yml down 
 docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api/docker-compose.yml up -d --remove-orphans

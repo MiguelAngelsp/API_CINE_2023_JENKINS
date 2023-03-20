5d05a11352646e045efeb158d96590fd8c7dc804
mMiguelAngelsp <miguel.correopers@gmail.com>
2023-03-20 10:14:48 +0100

    another commit

--- build/install.sh
+++ build/install.sh
@@ -5,7 +5,7 @@ export $(cat .env | xargs)
 # LEVANTAR LA API
 docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api/docker-compose.yml build --no-cache
 # docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api/docker-compose.yml down 
-docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api/docker-compose.yml up -d 
+docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api/docker-compose.yml up 
 #LEVANTAR LA APP
 docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/appcine/docker-compose.yml build --no-cache
 # docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pCine/api/docker-compose.yml down 
